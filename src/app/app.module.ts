fb29b9fa1f9382e8a42c0a141bab77687cf8cf43
JessieGumanab <Jessiegumanab@mail.fresnostate.edu>
2021-12-08 12:06:54 -0800

    Finished Final Project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { LoginComponent } from './login/login.component';
 import { BillsComponent } from './bills/bills.component';
 
 
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -24,6 +25,8 @@ import { BillsComponent } from './bills/bills.component';
     EditItemModalComponent,
     LoginComponent,
     BillsComponent
+  
+
   ],
   imports: [
     BrowserModule,
